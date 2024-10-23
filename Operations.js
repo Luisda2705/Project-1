2748555b4bc299328cfcdd2294ab7609c771bd47
Luis Arteaga <43306068+Luisda2705@users.noreply.github.com>
2024-10-22 20:35:52 -0600

    update

--- Operations.js
+++ Operations.js
@@ -1,6 +1,5 @@
- 
-let num1 = prompt();
-let num2 = prompt();
+let num1 = prompt("Please enter a number");
+let num2 = prompt("Please enter a number");
 
 const operations = function(num1, num2) {
     return num1+num2;
