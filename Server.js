bae6d7b36ad4811a8bb84e0fa4c89ee38247f421
Luis Arteaga <43306068+Luisda2705@users.noreply.github.com>
2024-10-22 22:26:56 -0600

    update

--- Server.js
+++ Server.js
@@ -1,10 +1,19 @@
-const express = require('express')
+const express = require('express');
 
-const app = express()
+const app = express();
 
-app.get('/', function (req, res) {
-  res.send('Hello World')
-})
+const port = process.env.PORT || 5000;
 
-app.listen(3000);
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  next();
+});
 
+app.use((req, res, next) => {
+  res.send('Welcome to Express');
+});
+
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
